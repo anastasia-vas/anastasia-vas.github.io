e69f9191fffe6eb20648ea88d7eb6623e2fed7bc
anastasia-vas <vasilevskayaan19@gmail.com>
2022-07-02 22:29:20 +0300

    Updated the look of the web-portfolio

--- web-portfolio/js/script.js
+++ web-portfolio/js/script.js
@@ -1,6 +1,17 @@
-new Vue({
-   el: '#app',
-   data: {
-      showMenu: false,
-   }
-})
+const { createApp, onMounted } = Vue;
+const App = {
+  el: "#app",
+  data() {
+    return {
+      openAbout: false,
+      activeSection: "home",
+    };
+  },
+  watch: {
+    activeSection(newActiveSection) {
+      this.activeSection = newActiveSection;
+    },
+  },
+};
+
+createApp(App).mount("#app");
